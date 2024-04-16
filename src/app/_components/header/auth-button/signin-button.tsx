e70673a1491731ef2c2c8e47f752891f3021ece8
AgusjkDev <agusjkdev7@gmail.com>
2024-04-16 02:54:38 -0300

    add signin button loading state

--- src/app/_components/header/auth-button/signin-button.tsx
+++ src/app/_components/header/auth-button/signin-button.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/client";
 
 export default function SignInButon() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const supabase = createClient();
 
     async function handleSignIn() {
+        setIsLoading(true);
+
         await supabase.auth.signInWithOAuth({
             provider: "google",
             options: { redirectTo: `${origin}/auth/callback` },
@@ -14,8 +20,16 @@ export default function SignInButon() {
     }
 
     return (
-        <Button variant="outline" onClick={handleSignIn}>
-            Sign in with Google
+        <Button className="w-40" disabled={isLoading} variant="outline" onClick={handleSignIn}>
+            {isLoading ? (
+                <>
+                    <Loader2 className="mr-2 size-4 animate-spin" />
+
+                    <span>Please wait</span>
+                </>
+            ) : (
+                "Sign in with Google"
+            )}
         </Button>
     );
 }
